e77a6b2304273df9ac12278383a0ce0efc6ef328
Syifaeka07 <syifaeka0709@gmail.com>
2022-04-16 14:47:40 +0700

    Menambahkan file htaccess, modifikasi autoload.php, menambahkan file Mahasiswa.php di dalam application/controllers, menambahkan file view-data-mahasiswa.php didalam application/views, menambahkan view-form-mahasiswa.php di dalam application/controllers

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -1,5 +1,5 @@
 <?php
-class Matakuliah extends CI_Controller
+class Mahasiswa extends CI_Controller
 {
  public function index()
  {
@@ -7,57 +7,16 @@ class Matakuliah extends CI_Controller
  }
  public function cetak()
  {
-    $this->form_validation->set_rules('Nama', 'Nama Mahasiswa', 
-    'required|min_length[3]', [
-     'required' => 'Nama Mahasiswa Harus diisi',
-     'min_lenght' => 'Nama terlalu pendek'
-     ]);
-     $this->form_validation->set_rules('NIS', 'NIS', 
-    'required|min_length[3]', [
-     'required' => 'NIS Harus diisi',
-     'min_lenght' => 'NIS terlalu pendek'
-     ]);
-     $this->form_validation->set_rules('Kelas', 'Kelas', 
-    'required|min_length[3]', [
-     'required' => 'Kelas Harus diisi',
-     'min_lenght' => 'Kelas terlalu pendek'
-     ]);
-     $this->form_validation->set_rules('Tanggal', 'Tanggal Lahir', 
-    'required|min_length[3]', [
-     'required' => 'Tanggal Lahir Harus diisi',
-     'min_lenght' => 'Tanggal Lahir terlalu pendek'
-     ]);
-     $this->form_validation->set_rules('Tempat', 'Tempat Lahir', 
-    'required|min_length[3]', [
-     'required' => 'Tempat Lahir Harus diisi',
-     'min_lenght' => 'Tempat Lahir terlalu pendek'
-     ]);
-     $this->form_validation->set_rules('', '', 
-    'required|min_length[3]', [
-     'required' => ' Harus diisi',
-     'min_lenght' => ' terlalu pendek'
-     ]);
-     $this->form_validation->set_rules('Alamat', 'Alamat', 
-    'required|min_length[3]', [
-     'required' => 'Alamat Harus diisi',
-     'min_lenght' => 'Alamat terlalu pendek'
-     ]);
-     $this->form_validation->set_rules('', '', 
-    'required|min_length[3]', [
-     'required' => ' Harus diisi',
-     'min_lenght' => ' terlalu pendek'
-     ]);
-     if ($this->form_validation->run() != true) {
-     $this->load->view(‘view-form-matakuliah’);
-     } else {
-    
  $data = [
- ‘kode’ => $this->input->post(‘kode’),
- ‘nama’ => $this->input->post(‘nama’),
- ‘sks’ => $this->input->post(‘sks’)
+ 'nama' => $this->input->post('nama'),
+ 'nis' => $this->input->post('nis'),
+ 'kelas' => $this->input->post('kelas'),
+ 'tanggal' => $this->input->post('tanggal'),
+ 'tempat' => $this->input->post('tempat'),
+ 'alamat' => $this->input->post('alamat'),
+ 'jenis' => $this->input->post('jenis'),
+ 'agama'  => $this->input->post('agama'),
  ];
- $this->load->view(‘view-data-matakuliah’, $data);
-  }
+ $this->load->view('view-data-mahasiswa', $data);
  }
 }
-
